67c1aeedcb8403f29385a6a192128f584e57b7a6
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-23 11:53:33 +0200

    done

--- Chapter 11 exercises/PayableInterfaceTest/PayableInterfaceTest.cs
+++ Chapter 11 exercises/PayableInterfaceTest/PayableInterfaceTest.cs
@@ -5,43 +5,20 @@ class PayableInterfaceTest
 {
     static void Main()
     {
-        var salariedEmployee = new SalariedEmployee("John", "Smith", "111-11-1111", 800.00m);
-        var hourlyEmployee = new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75m, 40.0m);
-        var commissionEmployee = new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000.00m, .06m);
-        var basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "4444-44-4444", 5000.00m, .04m, 300.00m);
-
-        Console.WriteLine("Employees processed individually:\n");
-
-        Console.WriteLine($"{salariedEmployee}\nearned: " +
-            $"{salariedEmployee.Earnings():C}\n");
-        Console.WriteLine($"{hourlyEmployee}\nearned: {hourlyEmployee.Earnings():C}\n");
-        Console.WriteLine($"{commissionEmployee}\nearned: " +
-            $"{commissionEmployee.Earnings():C}\n");
-        Console.WriteLine($"{basePlusCommissionEmployee}\nearned: " +
-            $"{basePlusCommissionEmployee.Earnings():C}\n");
-
-        var employees = new List<Employee>() { salariedEmployee, hourlyEmployee, commissionEmployee, basePlusCommissionEmployee };
-
-        Console.WriteLine("Employees processed polymorphically: \n");
-
-        foreach (var currentEmployee in employees)
+        var payableObjects = new List<IPayable>()
         {
-            Console.WriteLine(currentEmployee);
-            if (currentEmployee is BasePlusCommissionEmployee)
-            {
-                var employee = (BasePlusCommissionEmployee)currentEmployee;
+            new Invoice("01234", "seat", 2, 375.00m),
+            new Invoice("56789", "tire", 4, 79.95m),
+            new SalariedEmployee("john", "Smith", "111-11-1111", 800.00m),
+            new SalariedEmployee("Lisa", "Barnes", "888-88-8888", 1200.00m)};
 
-                employee.BaseSalary *= 1.10m;
-                Console.WriteLine("new base salary with 10% increase is: " +
-                    $"{employee.BaseSalary:C}");
-
-            }
-            Console.WriteLine($"earned: {currentEmployee.Earnings():C}\n");
-        }
-        for (int j = 0; j < employees.Count; j++)
+        Console.WriteLine(
+            "Invoices and Employees processed polymorphically:\n");
+        foreach (var payable in payableObjects)
         {
+            Console.WriteLine($"{payable}");
             Console.WriteLine(
-                $"Employee {j} is a {employees[j].GetType()}");
+                $"payment due: {payable.GetPaymentAmount():C}\n");
         }
 
         Console.ReadLine();
