ae0cb163f39a54e4f54cc6716174605dbe0eab19
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-27 09:54:26 +0200

    done with 12.9

--- Chapter 11 exercises/PayableInterfaceTest/Employee.cs
+++ Chapter 11 exercises/PayableInterfaceTest/Employee.cs
@@ -3,14 +3,26 @@
     public string FirstName { get; }
     public string LastName { get; }
     public string SocialSecurityNumber { get; }
-    private Date Birthday { get; }
+    private Date birthday;
 
-    public Employee(string firstName, string lastName, string socialSecurityNumber,Date birthday)
+    public Employee(string firstName, string lastName, string socialSecurityNumber, Date birthday)
     {
         FirstName = firstName;
         LastName = lastName;
         SocialSecurityNumber = socialSecurityNumber;
         Birthday = birthday;
+    }
+
+    public Date Birthday
+    {
+        get
+        {
+            return birthday;
+        }
+        set
+        {
+            birthday = value;
+        }
     }
 
     public override string ToString() => $"{FirstName} {LastName}\n" +
--- Chapter 11 exercises/PayableInterfaceTest/Test.cs
+++ Chapter 11 exercises/PayableInterfaceTest/Test.cs
@@ -5,11 +5,14 @@ class Test
 {
     static void Main()
     {
-        var birthday = new Date(1, 9, 1989);
+        var date1 = new Date(1, 1, 1989);
+        var birthday = new Date(2, 9, 1989);
         var salariedEmployee = new SalariedEmployee("John", "Smith", "111-11-1111", 800.00m, birthday);
         var hourlyEmployee = new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75m, 40.0m, birthday);
         var commissionEmployee = new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000.00m, .06m, birthday);
         var basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000.00m, .04m, 300.00m, birthday);
+        var pieceWorker2 = new PieceWorker("Brian", "Briansen", "555-55-5555", .40m, 1000, birthday);
+        birthday = new Date(4, 9, 1989);
         var pieceWorker = new PieceWorker("Brian", "Briansen", "555-55-5555", .40m, 1000, birthday);
 
         Console.WriteLine("Employees processed individually:\n");
@@ -25,23 +28,34 @@ class Test
         Console.WriteLine($"{pieceWorker}\nearned: " +
             $"{pieceWorker.Earnings():C}");
 
-        var employees = new List<Employee>() { salariedEmployee, hourlyEmployee, commissionEmployee, basePlusCommissionEmployee, pieceWorker };
+        var employees = new List<Employee>() { salariedEmployee, hourlyEmployee, commissionEmployee, basePlusCommissionEmployee, pieceWorker2, pieceWorker };
 
-        Console.WriteLine("Employees processed polymorphically: \n");
-
-        foreach (var currentEmployee in employees)
+        Console.WriteLine("\nEmployees processed polymorphically: \n");
+        for (int month = 1; month < 12; month++)
         {
-            Console.WriteLine(currentEmployee);
-            if (currentEmployee is BasePlusCommissionEmployee)
+            foreach (var currentEmployee in employees)
             {
-                var employee = (BasePlusCommissionEmployee)currentEmployee;
+                Console.WriteLine(currentEmployee);
+                if (currentEmployee is BasePlusCommissionEmployee)
+                {
+                    var employee = (BasePlusCommissionEmployee)currentEmployee;
 
-                employee.BaseSalary *= 1.10m;
-                Console.WriteLine("new base salary with 10% increase is: " +
-                    $"{employee.BaseSalary:C}");
+                    employee.BaseSalary *= 1.10m;
+                    Console.WriteLine("new base salary with 10% increase is: " +
+                        $"{employee.BaseSalary:C}");
+                }
+                if (month == currentEmployee.Birthday.Month)
+                {
+                    Console.WriteLine("Birthday Bonus: 100.00");
+                    Console.WriteLine($"earned {currentEmployee.Earnings() + 100:C}\n");
+                }
+                else
+                {
+                    Console.WriteLine($"earned {currentEmployee.Earnings():C}\n");
+                }
             }
-            Console.WriteLine($"earned {currentEmployee.Earnings():C}\n");
         }
+
 
         for (int j = 0; j < employees.Count; j++)
         {
